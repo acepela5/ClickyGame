3e9f788ff7b9d2a0980b1aa3b112914f615f225d
unknown <galan1451@gmail.com>
2019-09-17 19:49:38 -0500

    lines added

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const port = process.env.PORT || 8080;
 const app = express();
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use(express.static(__public));
 app.use(express.static(path.join(__public, 'build')));
 app.get('/ping', function (req, res) {
